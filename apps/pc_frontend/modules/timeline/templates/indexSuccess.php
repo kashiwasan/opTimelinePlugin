2b35d8033e2e8d0abe27068f6daacd9c95742f54
kashiwasan <kashiwagi@tejimaya.com>
2011-11-24 20:52:09 +0900

    icon追加

--- apps/pc_frontend/modules/timeline/templates/indexSuccess.php
+++ apps/pc_frontend/modules/timeline/templates/indexSuccess.php
@@ -23,7 +23,7 @@
         <div class="gorgon-row">
           <div class="gorgon-text"><a class="gorgon-screenname" href="<?php echo $baseUrl; ?>/member/${memberId}">${memberScreenName}</a> {{html body}} </div>
         </div>
-        <div class="gorgon-row"> <a href="#" class="timestamp"><span class="_timestamp">${createdAt}</span></a> <div class="delteLink" style="display: ${deleteLink}"><a href="#" rel="timelineDelete" class="timestamp" location-url="<?php echo $baseUrl ?>" data-activity-id="${id}" data-activity-body="${convertTag(body)}" data-activity-memberScreenName="${memberScreenName}" data-activity-csrftoken="<?php echo $token; ?>">Delete</a></div></div>
+        <div class="gorgon-row"> <div class="_reply-link" style="float: left; margin-right: 15px;"><a href="#" class="timestamp"><span class="_timestamp"><img src="<?php echo $baseUrl; ?>/opTimelinePlugin/css/images/clock.png" alt="timelineTimestampIcon" width="15" height="15" /> ${createdAt}</span></a></div> <div class="delteLink" style="display: ${deleteLink};"><a href="#" rel="timelineDelete" class="timestamp" location-url="<?php echo $baseUrl ?>" data-activity-id="${id}" data-activity-body="${convertTag(body)}" data-activity-memberScreenName="${memberScreenName}" data-activity-csrftoken="<?php echo $token; ?>"><img src="<?php echo $baseUrl; ?>/opTimelinePlugin/css/images/trash_can.png" alt="timelineDeleteIcon" width="15" height="15" /> Delete</a></div></div>
       </div>
       <div class="comment-list" id="commentlist-${id}">
       </div>
@@ -36,7 +36,7 @@
           <div class="comment-row">
             <div class="comment-text"><a class="comment-screenname">${memberScreenName}</a> {{html body}}</div>
           </div>
-          <div class="comment-row"> <a href="#" class="timestamp"><span class="_timestamp">${createdAt}</span> - Like!</a> <div class="delteLink" style="display: ${deleteLink}"><a href="#" rel="timelineDelete" class="timestamp" location-url="<?php echo $baseUrl ?>" data-activity-id="${id}" data-activity-body="${convertTag(body)}" data-activity-memberScreenName="${memberScreenName}" data-activity-csrftoken="<?php echo $token; ?>">Delete</a></div></div>
+          <div class="comment-row"> <div class="_reply-link" style="float: left; margin-right: 15px;"><a href="#" class="timestamp"><span class="_timestamp"><img src="<?php echo $baseUrl; ?>/opTimelinePlugin/css/images/clock.png" alt="timelineTimestampIcon" width="15" height="15" /> ${createdAt}</span> - Like!</a></div> <div class="delteLink" style="display: ${deleteLink};"><a href="#" rel="timelineDelete" class="timestamp" location-url="<?php echo $baseUrl ?>" data-activity-id="${id}" data-activity-body="${convertTag(body)}" data-activity-memberScreenName="${memberScreenName}" data-activity-csrftoken="<?php echo $token; ?>"><img src="<?php echo $baseUrl; ?>/opTimelinePlugin/css/images/trash_can.png" alt="timelineDeleteIcon" width="15" height="15" /> Delete</a></div></div>
         </div>
 </script>
 
