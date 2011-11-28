c91c5d6327d074bb7df31b4bac28a2bcde10525a
kashiwasan <pc.kashiwasan@gmail.com>
2011-11-28 15:03:18 +0900

    added plugin-install tasks.

---
+++ lib/task/opTimelinePluginInstallTask.class.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * This file is part of the OpenPNE package.
+ * (c) OpenPNE Project (http://www.openpne.jp/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file and the NOTICE file that were distributed with this source code.
+ */
+
+/**
+ * opTimelinePluginInstallTask
+ *
+ * @package    opTimelinePlugin
+ * @subpackage task
+ * @author     Shouta Kashiwagi <kashiwagi@tejimaya.com>
+ */
+
+class opTimelinePluginInstallTask extends sfBaseTask
+{
+  protected function configure()
+  {
+    $this->namespace        = 'opTimelinePlugin';
+    $this->name             = 'install';
+    $this->briefDescription = 'Install Command for "opTimelinePlugin".';
+ 
+    $this->detailedDescription = <<<EOF
+Use this command to install "opTimelinePlugin".
+EOF;
+  }
+ 
+  protected function execute($arguments = array(), $options = array())
+  {
+    // execute ./symfony plugin:publish-assets
+    $sfPluginPublishAssetsTask = new sfPluginPublishAssetsTask($this->dispatcher, $this->formatter);
+    $sfPluginPublishAssetsTask->run();
+
+    // execute ./symfomy cc
+    $sfCacheClearTask = new sfCacheClearTask($this->dispatcher, $this->formatter);
+    $sfCacheClearTask->run($arguments = array(), $options = array('type' => 'all'));
+  }
+}
