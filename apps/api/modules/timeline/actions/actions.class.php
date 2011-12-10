f91cdcacf3dcf3b794bd56c753857ed9ac7f54ea
kashiwasan <pc.kashiwasan@gmail.com>
2011-12-10 18:05:38 +0900

    added author credits

--- apps/api/modules/timeline/actions/actions.class.php
+++ apps/api/modules/timeline/actions/actions.class.php
@@ -1,5 +1,22 @@
 <?php
 
+/**
+ * This file is part of the OpenPNE package.
+ * (c) OpenPNE Project (http://www.openpne.jp/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file and the NOTICE file that were distributed with this source code.
+ */
+
+/**
+ * timeline actions.
+ *
+ * @package    OpenPNE
+ * @subpackage timeline
+ * @author     Shouta Kashiwagi <kashiwagi@tejimaya.com>
+ * @author     Yoichi Kimura <yoichi.kimura@tejimaya.com>
+ * @version    SVN: $Id: actions.class.php 9301 2008-05-27 01:08:46Z dwhittle $
+ */
 class timelineActions extends opApiActions
 {
   public function executeList(sfWebRequest $request)
@@ -12,6 +29,7 @@ class timelineActions extends opApiActions
       ->orderBy('ad.id DESC')
       ->limit(20)
       ->execute();
+
     $this->baseUrl = sfConfig::get('op_base_url');
     $this->viewMemberId = $this->getMember()->getId();
 
