dc3c6a862da597bf33acfdb19bca7e1e34b43b49
kashiwasan <pc.kashiwasan@gmail.com>
2012-01-13 09:01:17 +0900

    modified to use absolute url.(baseurl)

--- apps/pc_frontend/modules/timeline/templates/_smtTimelineCommunity.php
+++ apps/pc_frontend/modules/timeline/templates/_smtTimelineCommunity.php
@@ -6,7 +6,7 @@ var gorgon = {
     };
 //]]>
 </script>
-<script type="text/javascript" src="<?php echo url_for('@homepage'); ?>opTimelinePlugin/js/jquery.timeline.js"></script>
+<script type="text/javascript" src="<?php echo url_for('@homepage', array('absolute' => true)); ?>opTimelinePlugin/js/jquery.timeline.js"></script>
 <script type="text/javascript" src="<?php echo url_for('@homepage'); ?>opTimelinePlugin/js/gorgon-smt.js"></script>
 <script type="text/javascript">
   $(document).ready(function(){      
