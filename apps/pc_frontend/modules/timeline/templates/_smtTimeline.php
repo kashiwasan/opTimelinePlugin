3bbf0e9b352a9a70db8413e3f815ecb202ef7f4d
kashiwasan <pc.kashiwasan@gmail.com>
2012-03-01 20:05:37 +0900

    modified member home timeline style for bootstrap 2.0 (smartphone)

--- apps/pc_frontend/modules/timeline/templates/_smtTimeline.php
+++ apps/pc_frontend/modules/timeline/templates/_smtTimeline.php
@@ -9,42 +9,55 @@ var gorgon = {
 //]]>
 </script>
 <?php use_stylesheet('/opTimelinePlugin/css/colorbox.css') ?>
+<?php use_stylesheet('/opTimelinePlugin/css/timeline-smartphone.css', 'last') ?>
 <?php use_javascript('/opTimelinePlugin/js/jquery.colorbox.js', 'last') ?>
 <?php use_javascript('/opTimelinePlugin/js/jquery.timeline.js', 'last') ?>
-<?php use_javascript('/opTimelinePlugin/js/gorgon-smt.js', 'last') ?>
+<?php use_javascript('/opTimelinePlugin/js/timeline-loader-smartphone.js', 'last') ?>
 
 <script id="timelineTemplate" type="text/x-jquery-tmpl">
-<div class="timeline font14 row">
-  <div class="span2"><a href="<?php echo url_for('@homepage', array('absolute' => true)); ?>member/${member.id}"><img src="${member.profile_image}" class="rad6" width="46" height="46" /></a></div>
-  <div class="span10">
-    <div id="timelinebody-${id}" class="span10" style="min-height: 48px; max-width: 230px;">
-    <b><a href="${member.profile_url}">{{if member.screen_name}} ${member.screen_name} {{else}} ${member.name} {{/if}}</a></b><div class="timelinebody-context" id="timelinebody-context-${id}">{{html body}}</div></div>
-    <div id="commentlist-${id}"></div>
-  </div>
-</div>
-<div class="row span10" id="timeline-now-comment-${id}" style="margin-left: 50px;">
-  <button class="timeline-now-comment-button commentbutton btn span10 btn-mini" style="height: 20px; padding-top: 2px; padding-bottom: 2px; margin-left: 0px; margin-right: 0px; padding-left: 0px; padding-right: 0px;">コメントする</button>
-</div>
-<div class="row hide" id="timeline-comment-form-${id}">
-  <form class="span10 offset2" style="margin-bottom: 0px;">
-    <textarea class="span10 comment-textarea" data-timeline-id="${id}" style="height: 35px;" id="comment-textarea-${id}"></textarea>
-    <button data-timeline-id="${id}" class="btn btn-primary btn-mini timeline-comment-button center span10" style="height: 20px; padding: 1px; text-align: center;">投稿</button>
-  </form>
-</div>
+        <div class="timeline-post">
+          <div class="timeline-post-member-image">
+            <a href="${member.profile_url}"><img src="${member.profile_image}" alt="member-image" width="23" /></a>
+          </div>
+          <div class="timeline-post-content">
+            <div class="timeline-member-name">
+              <a href="${member.profile_url}">{{if member.screen_name}} ${member.screen_name} {{else}} ${member.name} {{/if}}</a>
+            </div>
+            <div class="timeline-post-body" id="timeline-body-context-${id}">
+              {{html body}}
+            </div>
+          </div>
+          <div class="timeline-post-control">
+            <a href="#" class="timeline-comment-link">コメントする</a>
+          </div>
+
+          <div class="timeline-post-comments" id="commentlist-${id}">
+
+            <div class="timeline-post-comment-form">
+            <input class="timeline-post-comment-form-input" type="text" data-timeline-id="${id}" id="comment-textarea-${id}" /><button data-timeline-id="${id}" class="btn btn-primary btn-mini timeline-comment-button">投稿</button>
+            </div>
+          </div>
+        </div>
 </script>
 
 <script id="timelineCommentTemplate" type="text/x-jquery-tmpl">
-<div class="comment font12 row">
-  <div class="span10">
-    <div class="row">
-      <div class="span1"><a href="${member.profile_url}"><img src="${member.profile_image}" class="rad6" width="23" height="23" /></a></div>
-      <div class="span9">
-        <b><a href="${member.profile_image}">{{if member.screen_name}} ${member.screen_name} {{else}} ${member.name} {{/if}}</a></b> 
-        <span id="timeline-comment-context-${id}" class="timeline-comment-context">{{html body}}</span>
-      </div>
-    </div>
-  </div>
-</div>
+            <div class="timeline-post-comment">
+
+              <div class="timeline-post-comment-member-image">
+                <a href="${member.profile_url}"><img src="${member.profile_image}" alt="" width="23" /></a>
+              </div>
+              <div class="timeline-post-comment-content">
+                <div class="timeline-post-comment-name-and-body">
+                <a href="${member.profile_url}">{{if member.screen_name}} ${member.screen_name} {{else}} ${member.name} {{/if}}</a>
+                <span class="timeline-post-comment-body">
+                {{html body}}
+                </span>
+                </div>
+              </div>
+              <div class="timeline-post-comment-control">
+              ${created_at}
+              </div>
+            </div>
 </script>
 
 <div style="display: none;">
@@ -62,12 +75,17 @@ var gorgon = {
   <div class="gadget_header span12">SNS全体のタイムライン</div>
 </div>
 
-<div id="timeline-list" class="span12 hide" data-last-id="" data-loadmore-id="" style="margin-left: 0px;">
+<div class="timeline">
+  <div id="timeline-list" data-last-id="" data-loadmore-id="" style="margin-left: 0px;">
+  
+  </div>
 </div>
+
+
 <div id="timeline-list-loader" class="row span12 center show" style="margin-top: 20px; margin-bottom: 20px;">
 <?php echo op_image_tag('ajax-loader.gif', array('alt' => 'Now Loading...')) ?>
 </div>
-<div id="gorgon-submit"></div>
+
 <hr class="toumei">
 <div class="row">
   <button class="span12 btn small" id="gorgon-loadmore">もっと読む</button>
---
+++ web/js/timeline-loader-smartphone.js
@@ -0,0 +1,173 @@
+$(function(){
+  var timerID;
+  timelineAllLoad();
+  timerID = setInterval('timelineDifferenceLoad()', 15000);
+ 
+  $('#tosaka_postform_submit').click( function() {
+    timelineDifferenceLoad();
+  });
+
+  $('#gorgon-loadmore').click( function() {
+    $('#timeline-list-loader').show();
+    $('#gorgon-loadmore').hide();
+    timelineLoadmore();
+    $('#timeline-list-loader').hide();
+    $('#gorgon-loadmore').show();
+  });
+
+});
+
+function timelineAllLoad() {
+  if (gorgon)
+  {
+    gorgon.apiKey = openpne.apiKey;
+    $.ajax({
+      type: 'GET',
+      url: openpne.apiBase + 'activity/search.json',
+      data: gorgon,
+      success: renderJSON,
+      error: function(XMLHttpRequest, textStatus, errorThrown){
+      $('#timeline-list-loader').hide();
+      $('#timeline-list').text('タイムラインは投稿されていません。');
+      $('#timeline-list').show();
+      },
+    });
+
+  }
+  else
+  {
+    $.ajax({
+      type: 'GET',
+      url: openpne.apiBase + 'activity/search.json?apiKey=' + openpne.apiKey,
+      success: renderJSON,
+      error: function(XMLHttpRequest, textStatus, errorThrown){
+      $('#timeline-list-loader').hide();
+      $('#timeline-list').text('タイムラインは投稿されていません。');
+      $('#timeline-list').show();
+      },
+    });
+  }
+}
+
+function renderJSON(json) {
+  $('#timeline-list').empty();
+  $timelineData = $('#timelineTemplate').tmpl(json.data);
+  $('button.timeline-comment-button', $timelineData).timelineComment();
+  $('.timeline-comment-link', $timelineData).click(function(){
+    $commentBoxArea = $(this).parent().siblings().find('.timeline-post-comment-form');
+    $commentBoxArea.show();
+    $commentBoxArea.children('.timeline-post-comment-form-input').focus();
+  }); 
+
+  $timelineData.appendTo('#timeline-list');
+  if(json.data[0])
+  {
+    $('#timeline-list').attr('data-last-id', json.data[0].id);
+  }
+  var max = json.data.length - 1;
+  if (json.data[max])
+  {
+    $('#timeline-list').attr('data-loadmore-id', json.data[max].id);
+  }
+  if(json.data)
+  for(i=0;i<json.data.length;i++)
+  {
+    if(json.data[i].replies)
+    {
+      $('#timelineCommentTemplate').tmpl(json.data[i].replies).appendTo('#commentlist-' +json.data[i].id);
+    }
+  }
+  $('a[rel^="timelineDelete"]').timelineDelete({callback: "timelineAllLoad()",});
+  $('#timeline-list-loader').hide();
+  $('#timeline-list').show();
+}
+
+function timelineDifferenceLoad() {
+  var lastId = $('#timeline-list').attr('data-last-id');
+  if (gorgon)
+  {
+    gorgon.apiKey = openpne.apiKey;
+  }
+  else
+  {
+    gorgon = {apiKey: openpne.apiKey,}
+  }
+  $.getJSON( openpne.apiBase + 'activity/search.json?count=20&since_id=' + lastId, gorgon, function (json) {
+    if (json.data[0])
+    {
+      $('#timeline-list').attr('data-last-id', json.data[0].id);
+    }
+    $timelineData = $('#timelineTemplate').tmpl(json.data);
+    $('button.timeline-comment-button', $timelineData).timelineComment();
+    $('.timeline-comment-link', $timelineData).click(function(){
+      $commentBoxArea = $(this).parent().siblings().find('.timeline-post-comment-form');
+      $commentBoxArea.show();
+      $commentBoxArea.children('.timeline-post-comment-form-input').focus();
+    }); 
+
+    $('#timeline-list').prepend($timelineData);
+    for(i=0;i<json.data.length;i++)
+    {
+      if(json.data[i].replies)
+      {
+        $('#timelineCommentTemplate').tmpl(json.data[i].replies).appendTo('#comment-list-' + json.data[i].id);
+      }
+    }
+  });
+}
+
+function timelineLoadmore() {
+  var loadmoreId = $('#timeline-list').attr('data-loadmore-id');
+  if (gorgon)
+  {
+    gorgon.apiKey = openpne.apiKey;
+  }
+  else
+  {
+    gorgon = {apiKey: openpne.apiKey,}
+  }
+
+  $('#timeline-list-loader').css('display', 'show');
+  $('#gorgon-loadmore').css('display', 'none');
+
+  $.getJSON( openpne.apiBase + 'activity/search.json?count=20&max_id=' + loadmoreId, gorgon, function (json) {
+    $('#timeline-list-loader').css('dispaly', 'show');
+    $('#gorgon-loadmore').css('display', 'none');
+    var max = json.data.length - 1;
+    if (max < 0) 
+    {
+      max = 0;
+    }
+    if (json.data[max])
+    {
+      $('#timeline-list').attr('data-loadmore-id', json.data[max].id);
+    }
+    $timelineData = $('#timelineTemplate').tmpl(json.data);
+    $('button.timeline-comment-button', $timelineData).timelineComment();
+    $('.timeline-comment-link', $timelineData).click(function(){
+      $commentBoxArea = $(this).parent().siblings().find('.timeline-post-comment-form');
+      $commentBoxArea.show();
+      $commentBoxArea.children('.timeline-post-comment-form-input').focus();
+    }); 
+
+    $('#timeline-list').after($timelineData);
+    for(i=0;i<json.data.length;i++)
+    {   
+      if(json.data[i].replies)
+      {   
+        $('#timelineCommentTemplate').tmpl(json.data[i].replies).appendTo('#commentlist-' +json.data[i].id);
+      }
+    }
+    $('#timeline-list-loader').css('display', 'none');
+    $('#gorgon-loadmore').css('display', 'show');
+  });
+}
+
+function convertTag(str) {
+  str = str.replace(/&/g,'&amp;');
+  str = str.replace(/"/g,'&quot;');
+  str = str.replace(/'/g,'&#039;');
+  str = str.replace(/</g,'&lt;');
+  str = str.replace(/>/g,'&gt;');
+  return str;
+}
