ce6690b0ce67aba5ad59a6b8b2a2e65a28f27570
kashiwasan <pc.kashiwasan@gmail.com>
2012-04-20 14:21:40 +0900

    fixed timeline smartphone javascript.

--- web/js/timeline-loader-smartphone.js
+++ web/js/timeline-loader-smartphone.js
@@ -1,7 +1,17 @@
 $(function(){
   var timerID;
+  var timerArray = new Array();
+  var timer;
   timelineAllLoad();
-  timerID = setInterval('timelineDifferenceLoad()', 10000);
+  if ( gorgon.timer != undefined )
+  {
+    timer = gorgon.timer;
+  }
+  else
+  {
+    timer = 15000;
+  }
+  timerID = setInterval('timelineDifferenceLoad()', timer);
  
   $('#tosaka_postform_submit').click( function() {
     setTimeout('timelineDifferenceLoad()', 1500);
@@ -25,11 +35,14 @@ function timelineAllLoad() {
       type: 'GET',
       url: openpne.apiBase + 'activity/search.json',
       data: gorgon,
-      success: renderJSON,
+      success: function (json){
+        renderJSON(json, 'all');
+        $('#timeline-list-loader').hide();
+      },
       error: function(XMLHttpRequest, textStatus, errorThrown){
-      $('#timeline-list-loader').hide();
-      $('#timeline-list').text('タイムラインは投稿されていません。');
-      $('#timeline-list').show();
+        $('#timeline-list-loader').hide();
+        $('#timeline-list').text('タイムラインは投稿されていません。');
+        $('#timeline-list').show();
       },
     });
 
@@ -39,48 +52,16 @@ function timelineAllLoad() {
     $.ajax({
       type: 'GET',
       url: openpne.apiBase + 'activity/search.json?apiKey=' + openpne.apiKey,
-      success: renderJSON,
+      success: function (json){
+        renderJSON(json, 'all');
+      },
       error: function(XMLHttpRequest, textStatus, errorThrown){
-      $('#timeline-list-loader').hide();
-      $('#timeline-list').text('タイムラインは投稿されていません。');
-      $('#timeline-list').show();
+        $('#timeline-list-loader').hide();
+        $('#timeline-list').text('タイムラインは投稿されていません。');
+        $('#timeline-list').show();
       },
     });
   }
-}
-
-function renderJSON(json) {
-  $('#timeline-list').empty();
-  $timelineData = $('#timelineTemplate').tmpl(json.data);
-  $('button.timeline-comment-button', $timelineData).timelineComment();
-  $('.timeline-comment-link', $timelineData).click(function(){
-    $commentBoxArea = $(this).parent().siblings().find('.timeline-post-comment-form');
-    $commentBoxArea.show();
-    $commentBoxArea.children('.timeline-post-comment-form-input').focus();
-  }); 
-
-  $timelineData.appendTo('#timeline-list');
-  if(json.data[0])
-  {
-    $('#timeline-list').attr('data-last-id', json.data[0].id);
-  }
-  var max = json.data.length - 1;
-  if (json.data[max])
-  {
-    $('#timeline-list').attr('data-loadmore-id', json.data[max].id);
-  }
-  if(json.data)
-  for(i=0;i<json.data.length;i++)
-  {
-    if(json.data[i].replies)
-    {
-      $('#timelineCommentTemplate').tmpl(json.data[i].replies).prependTo('#commentlist-' +json.data[i].id);
-      $('#timeline-post-comment-form-' + json.data[i].id, $timelineData).show();
-    }
-  }
-  $('a[rel^="timelineDelete"]').timelineDelete({callback: "timelineAllLoad()",});
-  $('#timeline-list-loader').hide();
-  $('#timeline-list').show();
 }
 
 function timelineDifferenceLoad() {
@@ -93,33 +74,14 @@ function timelineDifferenceLoad() {
   {
     gorgon = {apiKey: openpne.apiKey,}
   }
-  $.getJSON( openpne.apiBase + 'activity/search.json?count=20&since_id=' + lastId, gorgon, function (json) {
-    if (json.data[0])
-    {
-      $('#timeline-list').attr('data-last-id', json.data[0].id);
-    }
-    $timelineData = $('#timelineTemplate').tmpl(json.data);
-    $('button.timeline-comment-button', $timelineData).timelineComment();
-    $('.timeline-comment-link', $timelineData).click(function(){
-      $commentBoxArea = $(this).parent().siblings().find('.timeline-post-comment-form');
-      $commentBoxArea.show();
-      $commentBoxArea.children('.timeline-post-comment-form-input').focus();
-    }); 
-
-    $('#timeline-list').prepend($timelineData);
-    for(i=0;i<json.data.length;i++)
-    {
-      if(json.data[i].replies)
-      {
-        $('#timelineCommentTemplate').tmpl(json.data[i].replies).prependTo('#comment-list-' + json.data[i].id);
-        $('#timeline-post-comment-form-' + json.data[i].id, $timelineData).show();
-      }
-    }
+  $.getJSON( openpne.apiBase + 'activity/search.json?count=20&since_id=' + lastId, gorgon, function(json){
+    renderJSON(json, 'diff');
   });
 }
 
 function timelineLoadmore() {
   var loadmoreId = $('#timeline-list').attr('data-loadmore-id');
+  loadmoreId = loadmoreId - 1;
   if (gorgon)
   {
     gorgon.apiKey = openpne.apiKey;
@@ -128,42 +90,82 @@ function timelineLoadmore() {
   {
     gorgon = {apiKey: openpne.apiKey,}
   }
+  gorgon.max_id = loadmoreId;
 
-  $('#timeline-list-loader').css('display', 'show');
-  $('#gorgon-loadmore').css('display', 'none');
+  $.ajax({
+    type: 'GET',
+    url: openpne.apiBase + 'activity/search.json',
+    data: gorgon,
+    success: function(json){
+      renderJSON(json, 'more');
+      $('#timeline-loadmore-loading').hide();
+    },
+    error: function(XMLHttpRequest, textStatus, errorThrown){
+      $('#timeline-loadmore-loading').hide();
+    },  
+  }); 
+}
 
-  $.getJSON( openpne.apiBase + 'activity/search.json?count=20&max_id=' + loadmoreId, gorgon, function (json) {
-    $('#timeline-list-loader').css('dispaly', 'show');
-    $('#gorgon-loadmore').css('display', 'none');
-    var max = json.data.length - 1;
-    if (max < 0) 
+function renderJSON(json, mode) {
+  if (undefined == mode)
+  {
+    mode = 'all';
+  }
+  if ('all' == mode)
+  {
+    $('#timeline-list').empty();
+  }
+
+  $timelineData = $('#timelineTemplate').tmpl(json.data);
+  $('.timeline-comment-button', $timelineData).timelineComment();
+  $('.timeline-comment-link', $timelineData).click(function(){
+    $commentBoxArea = $(this).parent().siblings().find('.timeline-post-comment-form');
+    $commentBoxArea.show();
+    $commentBoxArea.children('.timeline-post-comment-form-input').focus();
+  });
+  if ('diff' == mode)
+  {
+    $timelineData.prependTo('#timeline-list');
+  }
+  else
+  {
+    $timelineData.appendTo('#timeline-list');
+  }
+  if ('all' == mode || 'diff' == mode)
+  {
+    if(json.data[0])
     {
-      max = 0;
+      $('#timeline-list').attr('data-last-id', json.data[0].id);
     }
+  }
+  if ('all' == mode || 'more' == mode)
+  {
+    var max = json.data.length - 1;
     if (json.data[max])
     {
       $('#timeline-list').attr('data-loadmore-id', json.data[max].id);
     }
-    $timelineData = $('#timelineTemplate').tmpl(json.data);
-    $('button.timeline-comment-button', $timelineData).timelineComment();
-    $('.timeline-comment-link', $timelineData).click(function(){
-      $commentBoxArea = $(this).parent().siblings().find('.timeline-post-comment-form');
-      $commentBoxArea.show();
-      $commentBoxArea.children('.timeline-post-comment-form-input').focus();
-    }); 
-
-    $('#timeline-list').after($timelineData);
+  }
+  if(json.data)
+  {
     for(i=0;i<json.data.length;i++)
-    {   
+    {
       if(json.data[i].replies)
-      {   
+      {
         $('#timelineCommentTemplate').tmpl(json.data[i].replies).prependTo('#commentlist-' +json.data[i].id);
         $('#timeline-post-comment-form-' + json.data[i].id, $timelineData).show();
       }
     }
-    $('#timeline-list-loader').css('display', 'none');
-    $('#gorgon-loadmore').css('display', 'show');
-  });
+  }
+  if ('all' == mode)
+  {
+    $('#timeline-loading').hide();
+  }
+  if ('more' == mode)
+  {
+    $('#timeline-loadmore').show();
+    $('#timeline-loadmore-loading').hide();
+  }
 }
 
 function convertTag(str) {
