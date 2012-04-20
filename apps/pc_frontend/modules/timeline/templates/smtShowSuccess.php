f5fb521bc73c9c0afc0cb86daa3acba7acf9c794
kashiwasan <pc.kashiwasan@gmail.com>
2012-04-20 17:24:42 +0900

    fixed 1 timeline page layout and style on smartphone.

--- apps/pc_frontend/modules/timeline/templates/smtShowSuccess.php
+++ apps/pc_frontend/modules/timeline/templates/smtShowSuccess.php
@@ -8,52 +8,70 @@ var gorgon = {
 //]]>
 </script>
 <?php use_stylesheet('/opTimelinePlugin/css/jquery.colorbox.css') ?>
+<?php use_stylesheet('/opTimelinePlugin/css/timeline-smartphone.css', 'last') ?>
 <?php use_javascript('/opTimelinePlugin/js/jquery.colorbox.js', 'last') ?>
 <?php use_javascript('/opTimelinePlugin/js/jquery.timeline.js', 'last') ?>
 <?php use_javascript('/opTimelinePlugin/js/timeline-loader-smartphone.js', 'last') ?>
 
 <script id="timelineTemplate" type="text/x-jquery-tmpl">
-<div class="timeline font10 row">
-  <div class="span2"><a href="${member.profile_url}"><img src="${member.profile_image}" class="rad6" width="46" height="46" /></a></div>
-  <div class="span10">
-    <div id="timelinebody-${id}" style="min-height: 48px;">
-    <b><a href="${member.profile_url}">{{if member.screen_name}} ${member.screen_name} {{else}} ${member.name} {{/if}}</a></b><br />
-    {{html body_html}}
-    </div>
-    <div id="commentlist-${id}"></div>
-  </div>
-</div>
-<div class="row span10" id="timeline-now-comment-${id}" style="margin-left: 50px;">
-  <button class="timeline-now-comment-button commentbutton btn span10 small" style="height: 20px;  padding-top: 2px; padding-bottom: 2px; margin-left: 0px; margin-right: 0px; padding-left: 0px; padding-right: 0px;">コメントする</button>
-</div>
-<div class="row hide" id="timeline-comment-form-${id}">
-  <form class="span10 offset2" style="margin-bottom: 0px;">
-    <textarea class="span10" style="height: 35px;" id="comment-textarea-${id}"></textarea>
-    <button data-timeline-id="${id}" class="btn primary small timeline-comment-button center span10" style="height: 20px; padding: 1px; text-align: center;">投稿</button>
-  </form>
-</div>
+        <div class="timeline-post">
+          <a name="timeline-${id}"></a>
+          <div class="timeline-post-member-image">
+            <a href="${member.profile_url}"><img src="${member.profile_image}" alt="member-image" width="23" /></a>
+          </div>
+          <div class="timeline-post-content">
+            <div class="timeline-member-name">
+              <a href="${member.profile_url}">{{if member.screen_name}} ${member.screen_name} {{else}} ${member.name} {{/if}}</a>
+            </div>
+            <div class="timeline-post-body" id="timeline-body-context-${id}">
+              {{html body_html}}
+            </div>
+          </div>
+
+          <div class="timeline-post-control">
+            <a href="#timeline-${id}" class="timeline-comment-link">コメントする</a>
+          </div>
+
+          <div class="timeline-post-comments" id="commentlist-${id}">
+
+            <div id="timeline-post-comment-form-${id}" class="timeline-post-comment-form">
+            <input class="timeline-post-comment-form-input" type="text" data-timeline-id="${id}" id="comment-textarea-${id}" /><button data-timeline-id="${id}" class="btn btn-primary btn-mini timeline-comment-button">投稿</button>
+            </div>
+            <div id="timeline-post-comment-form-loader-${id}" class="timeline-post-comment-form-loader">
+              <?php echo op_image_tag('ajax-loader.gif', array()) ?>
+            </div>
+          </div>
+
+
+        </div>
 </script>
 
 <script id="timelineCommentTemplate" type="text/x-jquery-tmpl">
-<div class="comment font10 row">
-  <div class="span10">
-    <div class="row">
-      <hr class="toumei2">
-      <div class="span1"><a href="${member.profile_url}"><img src="${member.profile_image}" class="rad6" width="23" height="23" /></a></div>
-      <div class="span9">
-      <b><a href="${member.profile_image}">{{if member.screen_name}} ${member.screen_name} {{else}} ${member.name} {{/if}}</a></b><br />
-      {{html body_html}}
-      </div>
-    </div>
-  </div>
-</div>
+            <div class="timeline-post-comment">
+
+              <div class="timeline-post-comment-member-image">
+                <a href="${member.profile_url}"><img src="${member.profile_image}" alt="" width="23" /></a>
+              </div>
+              <div class="timeline-post-comment-content">
+                <div class="timeline-post-comment-name-and-body">
+                <a href="${member.profile_url}">{{if member.screen_name}} ${member.screen_name} {{else}} ${member.name} {{/if}}</a>
+                <span class="timeline-post-comment-body">
+                {{html body_html}}
+                </span>
+                </div>
+              </div>
+              <div class="timeline-post-comment-control">
+              ${created_at}
+              </div>
+            </div>
+          </div>
 </script>
 
 <div class="row">
   <div class="gadget_header span12"><?php echo $activity->getMember()->getName(); ?>さんのタイムライン</div>
 </div>
 
-<div id="timeline-list" class="row span12 hide" data-post-baseurl="<?php echo url_for('@homepage'); ?>" data-last-id="" data-loadmore-id="">
+<div id="timeline-list" class="span12" data-post-baseurl="<?php echo url_for('@homepage'); ?>" data-last-id="" data-loadmore-id="">
 </div>
 <div id="timeline-list-loader" class="row span12 center show">
 <?php op_image_tag('ajax-loader.gif', array('absolute' => true)) ?>
