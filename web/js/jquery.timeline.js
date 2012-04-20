5e4cad3ffbbdbb0e92e5a645e8087c9f0e569b83
kashiwasan <pc.kashiwasan@gmail.com>
2012-04-20 14:36:26 +0900

    fixed for coding standards.

--- web/js/jquery.timeline.js
+++ web/js/jquery.timeline.js
@@ -12,13 +12,14 @@
       loader_path: '',
       callback: function(){},
     });
+
     return this.each(function(){
       $(this).click(function(){
         var id = $(this).attr("data-timeline-id");
         var foreign = $(this).attr('data-activity-foreign');
         var foreignId = $(this).attr('data-activity-foreign-id');
         var body = $('#comment-textarea-'+id).val();
-                                $timelineLoader = $(this).parent().next();
+        $timelineLoader = $(this).parent().next();
         $timelineLoader.next().text('');
         $timelineLoader.next().hide();
         $timelineLoader.show();
@@ -26,17 +27,20 @@
         $.ajax({
           url: openpne.apiBase + 'activity/post.json',
           type: 'POST',
-                data: 'body=' + body + '&in_reply_to_activity_id=' + id + '&apiKey=' + openpne.apiKey + '&target=' + foreign + '&target_id=' +foreignId,
-                dataType: 'json',
-                success: function(data) {
-                  if(data.status=='success'){
-                      $('#comment-textarea-'+id).val('');
-                      $timelineLoader.hide();
-                      timelineAllLoad();
-                  }else{
-                      alert(data.message);
-                  }
-                },
+          data: 'body=' + body + '&in_reply_to_activity_id=' + id + '&apiKey=' + openpne.apiKey + '&target=' + foreign + '&target_id=' +foreignId,
+          dataType: 'json',
+          success: function(data) {
+            if ('success' == data.status)
+            {
+              $('#comment-textarea-'+id).val('');
+              $timelineLoader.hide();
+              timelineAllLoad();
+            }
+            else
+            {
+              alert(data.message);
+            }
+          },
           error: function(x, r, t) {
             $timelineLoader.hide();
             $timelineLoader.next().text('投稿に失敗しました');
@@ -44,12 +48,14 @@
             $timelineLoader.prev().show();
           }
         });
+
         return false;
       });
     });
   };
 
   $.fn.timelineDelete = function() {
+
     return this.each(function(){
       $(this).click(function(){
       $(this).hide();
@@ -58,13 +64,13 @@
         $.ajax({
           url: openpne.apiBase + 'activity/delete.json',
           type: 'POST',
-                data: 'activity_id=' + activity_id + '&apiKey=' + openpne.apiKey,
-                dataType: 'json',
-                success: function(data) {
+          data: 'activity_id=' + activity_id + '&apiKey=' + openpne.apiKey,
+          dataType: 'json',
+          success: function(data) {
             $(this).parent().next().hide();
             $.colorbox.close();
             timelineAllLoad();
-                },
+          },
           error: function(x, r, t) {
             $(this).parent().next().hide();
             $.colorbox.close();
